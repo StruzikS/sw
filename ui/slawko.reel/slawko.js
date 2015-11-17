a07b24d7159e840e5f69e46e86dea2c6cc9a466b
StruzikS <noreply>
2015-11-17 09:56:35 +0000

    Add component slawko

---
+++ ui/slawko.reel/slawko.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/slawko.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Slawko
+ * @extends Component
+ */
+exports.Slawko = Component.specialize(/** @lends Slawko# */ {
+    constructor: {
+        value: function Slawko() {
+            this.super();
+        }
+    }
+});
